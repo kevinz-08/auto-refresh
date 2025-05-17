87481bb31f15ee9aa4d59fec5a821b21fdad8c78
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-05-17 18:08:41 -0500

    fix: :bug: Coreccion bugs de Funcionamiento

--- popup.js
+++ popup.js
@@ -1,15 +1,12 @@
 document.getElementById('start').addEventListener('click', async () => {
-  const interval = parseInt(document.getElementById('interval').value, 10) * 1000;
-
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: (ms) => {
-      if (window._refreshInterval) clearInterval(window._refreshInterval);
-      window._refreshInterval = setInterval(() => location.reload(), ms);
-    },
-    args: [interval]
+    func: () => {
+      if (window.autoRefresh) clearInterval(window.autoRefresh);
+      window.autoRefresh = setInterval(() => location.reload(), 3000);
+    }
   });
 });
 
@@ -19,10 +16,7 @@ document.getElementById('stop').addEventListener('click', async () => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: () => {
-      if (window._refreshInterval) {
-        clearInterval(window._refreshInterval);
-        delete window._refreshInterval;
-      }
+      clearInterval(window.autoRefresh);
     }
   });
 });
